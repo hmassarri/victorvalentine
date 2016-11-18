60c0fc1ebb2f36d83ee2771115893524c4923ab8
Hugo Massarri <hmassarri@Hugos-iMac.local>
2016-11-18 16:53:32 -0500

    initial commit

---
+++ contact.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>V I C T O R V A L E N T I N E</title>
+
+<style type="text/css" media="all">@import "http://www.hugomassarri.com/portfolio/valentine/css/hugo.css";</style>
+<link href="http://www.hugomassarri.com/portfolio/valentine/css/hugo.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+<body id="contact">
+	
+	
+<div id="topStrip">
+	
+	<div id="wrapper">
+		<div id="headerLeft"></div>
+		<div id="headerRight"><a href="contact.php">CONTACT</a></div>
+		<div id="headerRight"><a href="remixes.html">REMIXES</a></div>
+		<div id="headerRight"><a href="index.html">HOME</a></div>
+
+		<div id="mainPanel">
+			<div id="contact-area">
+			<!-- This goes first -->
+                        
+            <!-- This goes second (HTML FORM) -->
+            <form  method="post" "/portfolio/valentine/contact.php"> 
+            
+                <label for="Name">Email:</label>
+                <input type="text" name="Email" id="Email" />
+            	
+                <label for="Subject">Subject:</label>
+                <input type="text" name="Subject" id="Subject" />
+				<br />              
+            <label for="Message">Message:</label>
+                <textarea name="Message" rows="5" cols="20" id="Message"></textarea>
+                
+                                
+                <input type="submit" name="submit" value="Submit" class="submit-button" />
+            </form>
+			</div>
+		</div>
+
+		<div id="footer">
+			<div id="footerLeft">Copyright &copy; 2001-2011, All Rights Reserved, USA.</div>
+			<div id="footerRight"><img src="images/youtube-facebook-twitter2.png" border="0" /></div>
+		</div>
+
+	</div>
+	
+</div>
+
+<div id="middleStrip"></div>
+
+
+</body>
+</html>
